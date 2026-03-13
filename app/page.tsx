9da47bdf8eedd8fdbe5a23f92854a5c7b1cdddfb
nim550 <nimsarawijayasekara32@gmail.com>
2026-03-13 21:53:25 +0530

    change

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Nimsara Wijayasekara</h1>
+
+      <p>Bachelor of Software Engineering</p>
+
+      <p>University: OUSL</p>
+
+      <h2>Preferred Tech Stack</h2>
+
+      <ul>
+        <li>Frontend: React / Next.js</li>
+        <li>Backend: Flask</li>
+        <li>Project Management: Agile</li>
+      </ul>
+    </main>
+  );
+}
